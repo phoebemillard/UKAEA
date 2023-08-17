bb3c171f980b75abd073dbde99fa3d561b881cae
phoebemillard <104065346+phoebemillard@users.noreply.github.com>
2023-08-17 14:10:01 +0100

    Plots all peaks with noise removal

--- All_peaks.py
+++ All_peaks.py
@@ -8,53 +8,46 @@ fs = np.loadtxt('FrequencyAxis.dat')
 spectrum = np.loadtxt('SpectrumArray.dat')
 spectrum = np.log10(spectrum)  # makes it easier to see features without changing contrast
 
-p = np.argmin(abs(ts-(10.2)))
+p = np.argmin(abs(ts-(10.45)))
 array=[]
 old_peaks=[]
-k=p
-a=0
-while k<p+10:
+old_peaks2=[]
+k=2000
+q=0
+while k<2929:
 	smooth=savgol_filter(spectrum[:,k], window_length=15,  polyorder=3)
-	peaks=find_peaks(spectrum[:,k], distance=6,height=-3.7, width=3)
+	peaks=find_peaks(spectrum[:,k], distance=12,height=-3.7)
 	#peaks=find_peaks(smooth, distance=10.5,height=-3.7, width=5) #was at helight -3.8 and width 2
 	#plt.plot(spectrum[:,k], color='blue')
 	#plt.plot(smooth, color='red')
-	#plt.axhline(y=-3.5, color='yellow', linestyle='-')
+	#plt.axhline(y=-3.7, color='yellow', linestyle='-')
 	#plt.plot(peaks[0], spectrum[:,k][peaks[0]], color='yellow',marker='x', linestyle='none',label=k)
 	#plt.legend()
 	#plt.show()
 	
 	peak_freq=peaks[0]*(500/2048)
 	
-	#index for frames that have atleast on peak in 
-	if len(peak_freq)>0:
+	#removing some noise
+	a=0
+	while a<len(old_peaks):
+		b=0
+		while b<len(peak_freq):
+			c=0
+			while c<len(old_peaks2):
+				if abs(peak_freq[b]-old_peaks[a])<=1 or abs(old_peaks2[c]-old_peaks[a])<=1:
+					array.append([k-1,old_peaks[a]])
+					c=len(old_peaks2)
+					b=len(peak_freq)
+				else:
+					c=c+1
+			b=b+1
 		a=a+1
-	
-	i=0
-	while i<len(peak_freq):
-		array.append([a,k,peak_freq[i]])
-		i=i+1
-	
-	#print('\nold',old_peaks,'\nnew',peak_freq)
-	#while a<len(peak_freq):
-		#b=0
-		#while b<len(old_peaks):
-			#if abs(peak_freq[a]-old_peaks[b])<=0.3:
-				#array.append([k,peak_freq[a]])
-				#b=len(old_peaks)
-			#else:
-
-				#b=b+1
-		#a=a+1
-	#print('\narray',array,'\n\n')
-	#old_peaks=peak_freq
+			
+	old_peaks2=old_peaks
+	old_peaks=peak_freq
 	k=k+1
 
 np.savetxt('array.txt',array,delimiter=',')
-
-
-
-
 
 
 
@@ -68,6 +61,6 @@ plt.xlabel('$t$ $[s]$')
 plt.ylabel('$f$ $[kHz]$')
 plt.colorbar(label=r'Amplitude [a.u.]')
 while j<len(array):
-	plt.scatter(ts[array[j][1]],array[j][2], c='blue',marker='o',s=0.5)
+	plt.scatter(ts[array[j][0]],array[j][1], c='blue',marker='o',s=0.5)
 	j=j+1
 plt.show()
